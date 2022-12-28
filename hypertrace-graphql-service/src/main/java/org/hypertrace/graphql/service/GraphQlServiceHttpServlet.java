d1026c74e2999b3767abcbcdd71db61adf131f08
Prashant Pandey <prashant.pandey@razorpay.com>
2022-12-28 13:07:46 +0530

    log async timeout

--- hypertrace-graphql-service/src/main/java/org/hypertrace/graphql/service/GraphQlServiceHttpServlet.java
+++ hypertrace-graphql-service/src/main/java/org/hypertrace/graphql/service/GraphQlServiceHttpServlet.java
@@ -2,12 +2,17 @@ package org.hypertrace.graphql.service;
 
 import graphql.kickstart.servlet.GraphQLConfiguration;
 import graphql.kickstart.servlet.GraphQLHttpServlet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 class GraphQlServiceHttpServlet extends GraphQLHttpServlet {
+
+  private static final Logger LOG = LoggerFactory.getLogger(GraphQlServiceHttpServlet.class);
 
   private final GraphQLConfiguration configuration;
 
   GraphQlServiceHttpServlet(GraphQLConfiguration configuration) {
+    LOG.info("Async timeout set in graphql config is: {}", configuration.getAsyncTimeout());
     this.configuration = configuration;
   }
 
